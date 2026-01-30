3a02a333430615312e9ec5312fd3c1fc5fac5643
joyalk74 <joyalkuriakose74@gmail.com>
2026-01-30 16:35:21 +0530

    fixed docker file

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -22,14 +22,27 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+const isProduction = process.env.NODE_ENV === "production";
+
+// const corsOptions = {
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+// };
+
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: isProduction
+        ? true
+        : process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(helmet());
+
+
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
