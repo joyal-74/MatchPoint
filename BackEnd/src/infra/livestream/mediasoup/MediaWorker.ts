b36512cd2e87f959e896bf722466fdc619b23b18
joyalk74 <joyalkuriakose74@gmail.com>
2026-02-07 15:10:53 +0530

    fix streming ip address issue

--- BackEnd/src/infra/livestream/mediasoup/MediaWorker.ts
+++ BackEnd/src/infra/livestream/mediasoup/MediaWorker.ts
@@ -4,8 +4,8 @@ import { Worker } from "../Types.js";
 
 export const createMediaWorker = async (): Promise<Worker> => {
     return await mediasoup.createWorker({
-        rtcMinPort: 2000,
-        rtcMaxPort: 2020,
+        rtcMinPort: 10000,
+        rtcMaxPort: 10100,
         logLevel: "warn",
         logTags: ["info", "ice", "dtls", "rtp", "srtp", "rtcp"],
     });
